7dfbe28cd618b938f7d5347cbc4a0028b499a941
monkdmj <monkdmj@gmail.com>
2017-09-05 15:36:34 +0800

    11_2 commit

--- src/ContainMostWater_11.java
+++ src/ContainMostWater_11.java
@@ -26,6 +26,12 @@ public class ContainMostWater_11 {
     /** Now, to maximize the area, we need to consider the area between the lines of larger lengths.
      * If we try to move the pointer at the longer line inwards, we won't gain any increase in area,
      * since it is limited by the shorter line*/
+
+    /** Here is a simple proof for the solution.
+     * Use v[low, high] indicates the volume of container with low and high. suppose height[low] < height[high],
+     * then we move low to low+1, that means we ingored v[low, high-1],v[low, high-2],etc, if this is safe,
+     * then the algorithm is right, and it's obvious that v[low, high-1],high[low, high-2]......can't be
+     * larger than v[low, high] since its width can't be larger than high-low, and its height is limited by height[low].*/
     public int maxArea2(int[] height) {
         int maxarea = 0, l = 0, r = height.length - 1;
         while (l < r) {
